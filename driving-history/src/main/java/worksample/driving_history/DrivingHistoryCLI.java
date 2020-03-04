d524ad412ae6785d03af8353db25b7ece07d22df
B Gard <brandon@bgard.net>
2020-03-04 15:55:53 -0500

    Hello script

---
+++ driving-history/src/main/java/worksample/driving_history/DrivingHistoryCLI.java
@@ -0,0 +1,18 @@
+package worksample.driving_history;
+
+public class DrivingHistoryCLI {
+	
+	public void run() {
+		while(true) {
+			System.out.println("Hello");
+		}
+	}
+
+	public static void main(String[] args) {
+		DrivingHistoryCLI cli = new DrivingHistoryCLI();
+
+		cli.run();
+
+	}
+
+}
