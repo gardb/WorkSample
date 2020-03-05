f0715cedb02809052edaac40a02609c9891f36e0
B Gard <brandon@bgard.net>
2020-03-05 14:35:12 -0500

    Refactored file reader to continue if not Driver or Trip

--- driving-history/src/main/java/worksample/driving_history/driverdata/filereader/HistoryReader.java
+++ driving-history/src/main/java/worksample/driving_history/driverdata/filereader/HistoryReader.java
@@ -8,38 +8,40 @@ import java.util.Map;
 import worksample.driving_history.driverdata.Trip;
 
 public abstract class HistoryReader {
-	
+
 	public abstract Map<String, Trip> read();
-	
+
 	private String newDriver = "Driver";
 	private String newTrip = "Trip";
 	private BigDecimal zero = new BigDecimal(0);
-	
+
 	private String driver;
-	
+
 	protected Map<String, Trip> loadHistoryFromDelimitedLines(List<String> historyLines, String delimiter){
 		Map<String, Trip> driverHistory = new LinkedHashMap<String, Trip>();
-		
+
 		for (String historyLine : historyLines) {
 			String[] historyParts = historyLine.split(delimiter);
-			
+
 			driver = historyParts[1];
-			
+
 			if(historyParts[0].contentEquals(newDriver)) {
 				driverHistory.put(driver, new Trip(zero, zero, zero));
 			} else if (historyParts[0].contentEquals(newTrip)) {
-				driverHistory.put(driver, 
+				driverHistory.put(driver,
 						driverHistory.get(driver).calculateTrip(historyParts[2], historyParts[3], historyParts[4]));
+			} else {
+				continue;
 			}
 		}
-		
+
 		for (Map.Entry<String, Trip> entry : driverHistory.entrySet()) {
 			String key = entry.getKey();
 			Trip value = entry.getValue();
-			
+
 			System.out.println(key + value);
 		}
-		
+
 		return driverHistory;
 	}
 
