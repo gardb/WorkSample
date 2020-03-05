f364c8e859c03c5dc361e9b4158577ad7e339412
B Gard <brandon@bgard.net>
2020-03-05 13:38:15 -0500

    Refactored run

--- driving-history/src/main/java/worksample/driving_history/DrivingHistoryCLI.java
+++ driving-history/src/main/java/worksample/driving_history/DrivingHistoryCLI.java
@@ -5,28 +5,23 @@ import worksample.driving_history.driverdata.filereader.TxtHistoryReader;
 import worksample.driving_history.view.Menu;
 
 public class DrivingHistoryCLI {
-	
 	private Menu menu;
-	
+
 	public DrivingHistoryCLI(Menu menu) {
 		this.menu = menu;
 	}
-	
+
 	public void run() {
-			String dataFilePath = menu.getDataFilePath();
-			
-			while(true) {
-				HistoryReader reader = new TxtHistoryReader(dataFilePath);
-				
-				reader.read();
-				
-				break;
-			}
-		}
+		String dataFilePath = menu.getDataFilePath();
+
+		HistoryReader reader = new TxtHistoryReader(dataFilePath);
+
+		reader.read();
+	}
 
 	public static void main(String[] args) {
 		Menu menu = new Menu(System.in, System.out);
-		
+
 		DrivingHistoryCLI cli = new DrivingHistoryCLI(menu);
 
 		cli.run();
