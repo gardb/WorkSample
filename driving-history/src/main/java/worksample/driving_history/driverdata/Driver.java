d0d3565e195238857796fa24c709af3a99c2d149
B Gard <brandon@bgard.net>
2020-03-04 21:31:35 -0500

    Created mock data class

---
+++ driving-history/src/main/java/worksample/driving_history/driverdata/Driver.java
@@ -0,0 +1,14 @@
+package worksample.driving_history.driverdata;
+
+public class Driver {
+	private String name;
+	
+	public Driver(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+}
---
+++ driving-history/src/main/java/worksample/driving_history/driverdata/filereader/HistoryReader.java
@@ -0,0 +1,5 @@
+package worksample.driving_history.driverdata.filereader;
+
+public class HistoryReader {
+
+}
